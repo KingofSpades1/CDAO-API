202c772e4ed62c35236559a3678b21f8b1ebdf0d
kingofspades <kingofspades@protonmail.com>
2022-04-17 21:11:44 -0400

    CDAO-API
    api

---
+++ CDAO-API-main/routes/posts.js
@@ -0,0 +1,122 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const Post = require('../models/Post');
+
+//CREATE POST
+router.post('/', async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//UPDATE POST
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json('You can update only your post!');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE POST
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json('Post has been deleted...');
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json('You can delete only your post!');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET POST
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL POSTS
+router.get('/', async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username }).sort({ createdAt: -1 });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find().sort({ createdAt: -1 });
+      console.log(posts);
+    }
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//GET ALL WEB-3
+router.get('/cat/web3', async (req, res) => {
+  try {
+    let posts;
+    posts = await Post.find({ categories: 'WEB3' }).sort({ createdAt: -1 });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get('/cat/defi', async (req, res) => {
+  try {
+    let posts;
+    posts = await Post.find({ categories: 'DEFI' }).sort({ createdAt: -1 });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get('/cat/analytics', async (req, res) => {
+  try {
+    let posts;
+    posts = await Post.find({ categories: 'ANALYTICS' }).sort({ createdAt: -1 });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
